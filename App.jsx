a69a6d7afb32c894197b774478289d0c48123b88
Tundephilps <tundephilps@gmail.com>
2023-12-16 19:07:00 +0100

    Structure and Layout

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Intro from "./components/Intro";
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="auto" />
+      <Intro />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+});
---
+++ components/Intro.jsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Intro = () => {
+  return (
+    <SafeAreaView style={{}}>
+      <View
+        style={{
+          position: "relative",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          style={{
+            height: 30,
+            width: "80%",
+          }}
+          source={require("../assets/shuttlers.png")}
+          resizeMode="contain"
+        />
+        <Image
+          style={{
+            height: 180,
+            width: "100%",
+            position: "absolute",
+            bottom: "0%",
+          }}
+          source={require("../assets/buildings.jpg")}
+          resizeMode="cover"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Intro;
+
+const styles = StyleSheet.create({});
